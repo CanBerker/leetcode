a7fef772b4a35fc593d7d274e3ce8899e2e659ec
CanBerker <canberkerwork@gmail.com>
2019-10-06 02:51:31 +0300

    Add 283-MoveZeroes, simpler solution

--- 283-MoveZeroes/solution.js
+++ 283-MoveZeroes/solution.js
@@ -1,53 +1,16 @@
-
-const findZeroIndex = function (nums, current) {
-    while (current < nums.length) {
-        current++;
-        if (nums[current] === 0) {
-            break;
-        }
-    }
-    if (current === nums.length) {
-        return -1;
-    }
-    return current;
-}
-
-const findNumberIndex = function (nums, current) {
-    while (current < nums.length) {
-        current++;
-        if (nums[current] !== 0) {
-            break;
-        }
-    }
-    if (current === nums.length) {
-        return -1;
-    }
-    return current;
-}
-
-const swapNumberAndZero = function (nums, current, numberIndex) {
-    nums[current] = nums[numberIndex];
-    nums[numberIndex] = 0;
-}
-
 /**
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    let current = -1;
-    while (true) {
-        current = findZeroIndex(nums, current)
-        if (current === -1) {
-            return nums;
+    var numberInd = 0;
+    for (var i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[numberInd++] = nums[i];
         }
-
-        const numberIndex = findNumberIndex(nums, current);
-        if (numberIndex === -1) {
-            return nums;
-        }
-        
-        swapNumberAndZero(nums, current, numberIndex);
     }
-};
+    for (i = numberInd; i < nums.length; i++) {
+        nums[i] = 0;
+    }
+}
 
