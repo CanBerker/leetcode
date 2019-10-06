7b705d683a417116071af9e1ab27b2cec746a1ff
CanBerker <canberkerwork@gmail.com>
2019-10-06 15:05:57 +0300

    Add 189-RotateArray Circular rotation solution

--- 189-RotateArray/solution.js
+++ 189-RotateArray/solution.js
@@ -3,15 +3,15 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-// Use a new array
-// O(n) space O(n) time complexity
-var rotate = function(nums, k) {
-    k = k % nums.length;
-    const result = [...nums];
-    for (let i=0; i<result.length; i++) {
-        nums[(i+k)%result.length] = result[i];
-    }
-};
+// // Use a new array
+// // O(n) space O(n) time complexity
+// var rotate = function(nums, k) {
+//     k = k % nums.length;
+//     const result = [...nums];
+//     for (let i=0; i<result.length; i++) {
+//         nums[(i+k)%result.length] = result[i];
+//     }
+// };
 
 
 /**
@@ -53,4 +53,79 @@ var rotate = function(nums, k) {
 //         nums[i] = tmp;
 //     }
 // }
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+// // use reversing
+// // O(1) space complexity O(n) time complexity
+// var rotate = function(nums, k) {
+//     k = k % nums.length;
+//     reverseInPlace(nums, 0, nums.length-1-k);
+//     reverseInPlace(nums, nums.length-k, nums.length-1);
+//     reverseInPlace(nums, 0, nums.length-1)
+    
+// };
+// var reverseInPlace = function(nums, left, right) {
+//     let tmp;
+//     while(left< right) {
+//         tmp = nums[left];
+//         nums[left] = nums[right];
+//         nums[right] = tmp;
+//         left++;
+//         right--;
+//     }
+// }
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+// // Use a new array
+// // O(n) space O(n) time complexity
+// var rotate = function(nums, k) {
+//     k = k % nums.length;
+//     shiftAllByK(nums, k);
+// };
+// var shiftAllByK = function(nums, k) {
+//     for (let i=0; i<nums.length; i++) {
+//         console.log(`${i}, ${(i+k) % nums.length}`);
+//         swap(nums, i, (i+k) % nums.length);
+//     }
+// }
+// var swap = function(nums, i, j) {
+//     let tmp = nums[j];
+//     nums[j] = nums[i];
+//     nums[i] = tmp;
+// 
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+// Circular rotation
+// O(n) space O(n) time complexity
+var rotate = function(nums, k) {
+    k = k % nums.length;
+    let visited = 0;
+    for (let start = 0; visited < nums.length; start++) {
+        let current = start;
+        let prevValue = nums[start];
+        do{
+            let nextIndex = (current + k) % nums.length;
+            let tmp = nums[nextIndex];
+            nums[nextIndex] = prevValue;
+            prevValue = tmp;
+            current = nextIndex;
+            visited++;
+        } while(current != start)
+    }
+};
+
 
