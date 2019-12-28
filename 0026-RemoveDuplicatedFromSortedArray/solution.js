34eaa0c9a2aebef9d30c0f39650c30cec59ec5db
CanBerker <canberkerwork@gmail.com>
2019-12-28 17:02:13 +0100

    Update 0026

--- 0026-RemoveDuplicatedFromSortedArray/solution.js
+++ 0026-RemoveDuplicatedFromSortedArray/solution.js
@@ -2,16 +2,32 @@
  * @param {number[]} nums
  * @return {number}
  */
+
+// use "already sorted" property, just check previous numn
 var removeDuplicates = function(nums) {
-    let seen = Object.create(null);
-    let index = 0;
-    for (let n of nums) {
-        if (seen[n] === undefined) {
-            seen[n] = 1;
-            nums[index] = n;
-            index++;
+    if(nums.length <= 1) {
+        return nums.length;
+    }
+    var k = 1;
+    for(var i = 1; i < nums.length; i++) {
+        if(nums[i] != nums[i-1]){
+            nums[k++] = nums[i];
         }
     }
-    return index;
+    return k;
 };
+
+// can solution
+// var removeDuw plicates = function(nums) {
+//     let seen = Object.create(null);
+//     let index = 0;
+//     for (let n of nums) {
+//         if (seen[n] === undefined) {
+//             seen[n] = 1;
+//             nums[index] = n;
+//             index++;
+//         }
+//     }
+//     return index;
+// };
 
