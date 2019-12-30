943e775f59748a1b53d460947703abba732846a9
CanBerker <canberkerwork@gmail.com>
2019-12-30 13:03:39 +0100

    Add js solution 0003

---
+++ 0003-LongestSubstrWithoutRepeatingChars/solution.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+// sliding window optimized (immediately update start as duplicate +1)
+var lengthOfLongestSubstring = function(s) {
+    let hashMap = {}, start = 0, max = 0;
+    for (let i=0;i<s.length;i++) {
+        if (hashMap[s[i]]!=null) {
+            if (hashMap[s[i]] + 1 > start) { // if the character is after start
+                start = hashMap[s[i]] + 1; // update start position
+            }
+        }
+        hashMap[s[i]] = i; // update char position
+        max = (i-start+1)>max?(i-start+1):max; // update max if neccessary
+    }
+    return max;
+}
+
+
