1f44256abc64d3ba456acd868814bd60df9a62fd
CanBerker <canberkerwork@gmail.com>
2019-12-30 11:59:48 +0100

    Update 0028 with more js solutions

--- 0028-ImplementStringSearch/solution.js
+++ 0028-ImplementStringSearch/solution.js
@@ -27,10 +27,9 @@ var strStr = function(haystack, needle) {
 //     return haystack.indexOf(needle);
 // };
 
-// knuth morris prath implementation
+// knuth morris prath implementation - keep a kmax of needle and fallback to the correct location upon mismatch
 // O(m) preprocessing and space, O(n) time complexity
 
-// Boyer moore implementation
+// Boyer moore implementation - slide the needle over the haystack
 // O(m+k) preprocessing, O(k)space, O(nm) worst - O(n/m)  best time complexity
-
 
